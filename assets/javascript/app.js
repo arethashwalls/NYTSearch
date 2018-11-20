31628c760843a223cdc8533500610605ee874193
Aretha S. H. Walls <43796460+arethashwalls@users.noreply.github.com>
2018-11-19 21:18:44 -0700

    Update app.js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,16 +1,18 @@
-$( document ).ready(function() {
-    var key = '066443b5ae6e4b4a9d41cb76e219d811';
-    var topic = 'cat';
-    var recordCount = 5;
-    var startDate = 20180101;
-    var endDate = 20181119;
+var key = '066443b5ae6e4b4a9d41cb76e219d811';
+var topic = 'cat';
+var recordCount = 5;
 
+$(".searchButton").on("click", function () {
+   var startDate = $(".start-year").val();
+   var endDate = $(".end-year").val();
+   var topic = $(".topic").val();
 
-    $.ajax({
-        url: "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=" + key + "&q=" +  topic + 
-            "&begin_date=" + startDate + "&end_date=" + endDate,
-        method: 'GET'
-    }).then(function(response){
-        console.log(response);
-    });
+   $.ajax({
+       url: "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=" + key + "&q=" + topic +
+           "&begin_date=" + startDate + "&end_date=" + endDate,
+       method: 'GET'
+   }).then(function (response) {
+       console.log(response);
+   });
+
 });
