ce270e50d53ddb9e5c21dcd968369b2778362fb9
ASH Walls <ariswalls@gmail.com>
2018-11-19 21:24:13 -0700

    commit

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,13 +1,13 @@
 var key = '066443b5ae6e4b4a9d41cb76e219d811';
 var topic = 'cat';
 var recordCount = 5;
-
+console.log('hi')
 $(".searchButton").on("click", function (e) {
    e.preventDefault();
    var startDate = $(".start-year").val();
    var endDate = $(".end-year").val();
    var topic = $(".topic").val();
-
+    
    $.ajax({
        url: "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=" + key + "&q=" + topic +
            "&begin_date=" + startDate + "&end_date=" + endDate,
