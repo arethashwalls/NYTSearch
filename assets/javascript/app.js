430acb157bf11eca3ea1cc2474be0396258ed693
Aretha S. H. Walls <43796460+arethashwalls@users.noreply.github.com>
2018-11-19 21:23:58 -0700

    Update app.js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,19 +1,21 @@
-var key = '066443b5ae6e4b4a9d41cb76e219d811';
-var topic = 'cat';
-var recordCount = 5;
+$( document ).ready(function() {
+   var key = '066443b5ae6e4b4a9d41cb76e219d811';
+   var topic = 'cat';
+   var recordCount = 5;
 
-$(".searchButton").on("click", function (e) {
-   e.preventDefault();
-   var startDate = $(".start-year").val();
-   var endDate = $(".end-year").val();
-   var topic = $(".topic").val();
+   $(".searchButton").on("click", function (e) {
+      e.preventDefault();
+      var startDate = $(".start-year").val();
+      var endDate = $(".end-year").val();
+      var topic = $(".topic").val();
 
-   $.ajax({
-       url: "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=" + key + "&q=" + topic +
-           "&begin_date=" + startDate + "&end_date=" + endDate,
-       method: 'GET'
-   }).then(function (response) {
-       console.log(response);
+      $.ajax({
+          url: "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=" + key + "&q=" + topic +
+              "&begin_date=" + startDate + "&end_date=" + endDate,
+          method: 'GET'
+      }).then(function (response) {
+          console.log(response);
+      });
+
    });
-
 });
